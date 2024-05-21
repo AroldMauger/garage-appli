34e90d1622424eb1f141016a88a72015e9988063
Arold Mauger <aroldmauger2022@gmail.com>
2024-05-21 21:16:27 +0200

    Récupération des données depuis l'API et affichage des items dans le dashboard

--- app/src/main/java/com/garagetrempu/android/dashboard/appointmentlist/AppointmentAdapter.kt
+++ app/src/main/java/com/garagetrempu/android/dashboard/appointmentlist/AppointmentAdapter.kt
@@ -1,16 +1,38 @@
 package com.garagetrempu.android.dashboard.appointmentlist
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.garagetrempu.android.R
 import com.garagetrempu.android.response.GetAppointmentsResponse
+import kotlinx.android.synthetic.main.item_appointment.view.appointmentCar
+import kotlinx.android.synthetic.main.item_appointment.view.appointmentDate
+import kotlinx.android.synthetic.main.item_appointment.view.appointmentReason
+import kotlinx.android.synthetic.main.item_appointment.view.appointmentTime
+import kotlinx.android.synthetic.main.item_appointment.view.seeMore
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class AppointmentAdapter (val items:List<GetAppointmentsResponse>): RecyclerView.Adapter<AppointmentAdapter.ViewHolder> () {
 
     class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
-
+        @SuppressLint("SetTextI18n")
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun bind(appointment: GetAppointmentsResponse){
+            val date = LocalDateTime.parse(appointment.date, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"))
+            itemView.appointmentDate.text = "${date.year}/${date.month.value}/${date.dayOfMonth}"
+            itemView.appointmentTime.text = "${date.hour}h${date.minute}"
+            itemView.appointmentCar.text = appointment.car
+            itemView.appointmentReason.text = appointment.reason
+            itemView.seeMore.setOnClickListener{
+                var toto = ""
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_appointment, parent, false))
@@ -18,6 +40,6 @@ class AppointmentAdapter (val items:List<GetAppointmentsResponse>): RecyclerView
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // récupérer l'item avec sa position et bind le tout
+        holder.bind(items[position])
     }
 }
